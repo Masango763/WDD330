2095de74b74293e317d1e5c5da428b9f1489da49
Masango763 <dewhemasango@gmail.com>
2026-09-11 04:56:21 +0200

    Fix empty cart error on cart.html

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,31 @@
-import ShoppingCart from "./ShoppingCart.mjs";
+import { getLocalStorage } from "./utils.mjs";
 
-const cart = new ShoppingCart("so-cart", ".product-list");
-cart.renderCartContents();
+function cartItemTemplate(item) {
+  return `<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
+    <img src="${item.Image}" alt="${item.Name}" />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${item.Name}</h2>
+  </a>
+  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">$${item.FinalPrice}</p>
+</li>`;
+}
+
+function renderCartContents() {
+  const cartItems = getLocalStorage("so-cart");
+  const element = document.querySelector(".product-list");
+
+  if (!cartItems || cartItems.length === 0) {
+    element.innerHTML =
+      '<li class="cart-empty"><p>Your cart is currently empty.</p></li>';
+    return;
+  }
+
+  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  element.innerHTML = htmlItems.join("");
+}
+
+renderCartContents();
