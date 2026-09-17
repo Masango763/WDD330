9bec64e0666cc6975d5d9d308fbed10fb4d862e1
Masango763 <dewhemasango@gmail.com>
2026-09-17 22:43:09 +0200

    Complete W03 Expand Inventory activity requirements and API integrations

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,5 +1,16 @@
 import { renderListWithTemplate } from './utils.mjs';
 
+function productCardTemplate(product) {
+  return `<li class="product-card">
+    <a href="/product_pages/index.html?product=${product.Id}">
+      <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
+      <h3 class="card__brand">${product.Brand.Name}</h3>
+      <h2 class="card__name">${product.Name}</h2>
+      <p class="product-card__price">$${product.FinalPrice}</p>
+    </a>
+  </li>`;
+}
+
 export default class ProductList {
   constructor(category, dataSource, listElement) {
     this.category = category;
@@ -10,24 +21,15 @@ export default class ProductList {
   async init() {
     const list = await this.dataSource.getData(this.category);
     this.renderList(list);
-    const titleCategory = document.querySelector('.title-category');
-    if (titleCategory) {
-      titleCategory.textContent = this.category.charAt(0).toUpperCase() + this.category.slice(1).replace('-', ' ');
+    
+    const titleElement = document.querySelector('.title');
+    if (titleElement && this.category) {
+      const formattedCategory = this.category.charAt(0).toUpperCase() + this.category.slice(1).replace('-', ' ');
+      titleElement.innerHTML = `Top Products: ${formattedCategory}`;
     }
   }
 
   renderList(list) {
-    renderListWithTemplate(productCardTemplate, this.listElement, list);
+    renderListWithTemplate(productCardTemplate, this.listElement, list, "afterbegin", true);
   }
-}
-
-function productCardTemplate(product) {
-  return `<li class="product-card">
-    <a href="/product_pages/index.html?product=${product.Id}">
-      <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
-      <h3 class="card__brand">${product.Brand.Name}</h3>
-      <h2 class="card__name">${product.NameWithoutBrand}</h2>
-      <p class="product-card__price">$${product.FinalPrice}</p>
-    </a>
-  </li>`;
 }
