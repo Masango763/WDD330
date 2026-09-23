4ef1ebea8afd06a7d805501f69180d8232982e52
Masango763 <dewhemasango@gmail.com>
2026-09-23 22:59:35 +0200

    feat(w04): implement form validation, convertToJson error handling, alert utilities, and order success page

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,23 +1,34 @@
 const baseURL = import.meta.env.VITE_SERVER_URL || "https://wdd330-backend.onrender.com/";
 
-function convertToJson(res) {
+async function convertToJson(res) {
+  const jsonResponse = await res.json();
   if (res.ok) {
-    return res.json();
+    return jsonResponse;
   } else {
-    throw new Error("Bad Response");
+    throw { name: "servicesError", message: jsonResponse };
   }
 }
 
 export default class ExternalServices {
   async getData(category) {
     const response = await fetch(`${baseURL}products/search/${category}`);
-    const data = await convertToJson(response);
-    return data.Result;
+    return convertToJson(response);
   }
 
   async findProductById(id) {
     const response = await fetch(`${baseURL}product/${id}`);
-    const data = await convertToJson(response);
-    return data.Result;
+    return convertToJson(response);
+  }
+
+  async checkout(payload) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    };
+    const response = await fetch(`${baseURL}checkout`, options);
+    return convertToJson(response);
   }
 }
